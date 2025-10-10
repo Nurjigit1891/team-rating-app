19752cf8924336b68732349fb5faa3440ae2f034
Nurjigit1891 <117098636+Nurjigit1891@users.noreply.github.com>
2025-10-10 13:58:55 +0300

    Разделил коллекцию в БД потом сделал отдельную страницу для друзей и для одноклубников

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -3,14 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import App from '../../App'
 import AdminDashboard from '../adminDashboard/AdminDashboard'
 import FriendRatingApp from '../friendRating/FriendRatingApp'
+import AdminDashboardFriends from '../adminDashboardFriends/AdminDashboardFriends'
 
 const Layout = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<App />}/>
-        <Route path='friends' element={<FriendRatingApp />} />
+        <Route path='friends' elem  ent={<FriendRatingApp />} />
         <Route path='/admin123' element={<AdminDashboard />}/>
+        <Route path='/admin-friends' element={<AdminDashboardFriends />} />
       </Routes>
     </div>
   )
