451a1dad60164e912ce713f3d07135164e62ec4d
Nurjigit1891 <117098636+Nurjigit1891@users.noreply.github.com>
2025-10-10 12:12:23 +0300

    Добавил новую страницу для друзей

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import App from '../../App'
 import AdminDashboard from '../adminDashboard/AdminDashboard'
+import FriendRatingApp from '../../friendRating/FriendRatingApp'
 
 const Layout = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<App />}/>
+        <Route path='friends' element={<FriendRatingApp />} />
         <Route path='/admin123' element={<AdminDashboard />}/>
       </Routes>
     </div>
